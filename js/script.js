94567efecf8d3b8b15709968cefd5f8425249f18
alexiaev20 <alexiaevellyn20@gmail.com>
2024-10-12 19:48:46 -0300

    subindo Projeto

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+// Script básico para ações de botão (se necessário no futuro)
+document.querySelector('.cta-btn').addEventListener('click', () => {
+    alert('Entrando em contato...');
+  });
+  
