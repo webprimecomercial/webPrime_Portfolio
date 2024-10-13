69c99e0a4e611d852973c5e3dbeaa0acb01311f4
Gilkleyton Rodrigues <gilkleyton.andrade@gmail.com>
2024-10-12 22:11:41 -0300

    conteudo da sessão projetos

--- js/script.js
+++ js/script.js
@@ -3,4 +3,28 @@
 document.querySelector('.cta-btn').addEventListener('click', () => {
     alert('Entrando em contato...');
   });
+
+
+  // Script da sessão de projetos
+const filtros = document.querySelectorAll('.filtro');
+const projetos = document.querySelectorAll('.portfolio');
+
+filtros.forEach(filtro => {
+    filtro.addEventListener('click', function() {
+      // Remove a classe ativo de todos os botões
+      filtros.forEach(btn => btn.classList.remove('ativo'));
+      // Adiciona a classe ativo ao botão clicado
+      this.classList.add('ativo');
+
+      const categoria = this.getAttribute('data-filter');
+
+      projetos.forEach(portfolio => {
+        if (categoria === 'todos' || portfolio.getAttribute('data-category') === categoria){
+          portfolio.style.display = 'block';
+        } else {
+          portfolio.style.display = 'none';
+        }
+      });
+    });
+});
   
